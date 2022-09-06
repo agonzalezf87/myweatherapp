829d4a37b78cebc0d536d0f6547e5ba927522d1f
Antonio Gonzalez <agonzalezf87@gmail.com>
2022-09-06 16:54:21 -0500

    [add] Grid dashboard with mobile layout. Pending responsiveness.

---
+++ src/containers/Dashboard.jsx
@@ -0,0 +1,19 @@
+import '../styles/containers/Dashboard.sass'
+
+const Dashboard = () => {
+  return (
+    <section className="Dashboard">
+      <div className="item current">item1</div>
+      <div className="item hourly">item2</div>
+      <div className="item map">item3</div>
+      <div className="item daily">item4</div>
+      <div className="item saved">item5</div>
+      <div className="item">item6</div>
+      <div className="item">item7</div>
+      <div className="item">item8</div>
+      <div className="item">item9</div>
+    </section>
+  )
+}
+
+export { Dashboard }
--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,10 +1,11 @@
+import { Dashboard } from './Dashboard'
 import '../styles/containers/Main.sass'
 
 const Main = () => {
   return (
     <main className="Main">
       <section className='Main__content'>
-        <h1>This Will Be The Main Container</h1>
+        <Dashboard />
       </section>
     </main>
   )
